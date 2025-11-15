9fbcfbe493343712460581079832690323a1aafc
Maximilian Blazek <68336485+dgtlntv@users.noreply.github.com>
2025-11-15 17:11:48 +0100

    make brand color be based on config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,38 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+    content: [
+        "./public/*.html",
+        "./app/helpers/**/*.rb",
+        "./app/javascript/**/*.js",
+        "./app/views/**/*.{erb,haml,html,slim}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                brand: {
+                    50: "#fef2f3",
+                    100: "#fee2e4",
+                    200: "#fecace",
+                    300: "#fda4aa",
+                    400: "#f97079",
+                    500: "#ef3340",
+                    600: "#dd2532",
+                    700: "#ba1b26",
+                    800: "#9a1a23",
+                    900: "#801c23",
+                    950: "#450a0e",
+                },
+            },
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/aspect-ratio"),
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/container-queries"),
+    ],
 }
